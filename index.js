c686823fdaf8746322bb5253ecd3203723e7f6c0
Ryan-Wee-yh <ryanwyh05@gmail.com>
2022-12-04 23:02:57 +0800

    Final polish and checks for JS files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-navbar = document.querySelector(".navbar");
+navbar = document.querySelector(".navbar");              /* helps initiate custom dropdown for nav bar in phone version */
 navbar.onclick = function() {
   navBar = document.querySelector(".nav-bar");
   navBar.classList.toggle("active")
